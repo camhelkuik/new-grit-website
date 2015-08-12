8a6bcac76f86f1955709e5a4f7abf99fc0fc9a4c
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-08-11 21:43:45 -0500

    attempting to fix js media query

--- global.js
+++ global.js
@@ -1,47 +1,47 @@
 // Sticky header--------------------------------
   
-  if (matchMedia) {
-  	var mq = window.matchMedia("(max-width: 554px)");
-  	mq.addListener(WidthChange);
-  	WidthChange(mq);
-  }
+if (matchMedia) {
+  var mq = window.matchMedia("(max-width: 554px)");
+  mq.addListener(WidthChange);
+  WidthChange(mq);
+}
   
-  // media query change
-  function WidthChange(mq) {
+// media query change
+function WidthChange(mq) {
 
-    var nav =  document.getElementById("sticky_nav");
-    var header = document.getElementById("sticky_header");
+  var nav =  document.getElementById("sticky_nav");
+  var header = document.getElementById("sticky_header");
 
-    window.onscroll = shrink_header;
+  window.onscroll = shrink_header;
+    
 
-  	if (mq.matchMedia) {
-  		// window width is at least 500px
-      function shrink_header() {
-          if (window.pageYOffset>115) {
-            header.style.top = "0px";
-            nav.style.top = "-55px";
-          }
-          if (window.pageYOffset<115) {
-            header.style.top = "-150px";
-            nav.style.top = "-150px";
-          }
-        }
-  	}
-  	else {
-  		// window width is less than 500px
-      function shrink_header() {
-           if (window.pageYOffset>115) {
-             header.style.top = "0px";
-             nav.style.top = "-29px";
-           }
-           if (window.pageYOffset<115) {
-             header.style.top = "-150px";
-             nav.style.top = "-150px";
-           }
-         }
-  	}
-
+  if (mq.matchMedia) {
+    // window width is at least 500px
+    function shrink_header() {
+      if (window.pageYOffset>115) {
+        header.style.top = "0px";
+        nav.style.top = "-55px";
+      }
+      if (window.pageYOffset<115) {
+        header.style.top = "-150px";
+        nav.style.top = "-150px";
+      }
+    }
   }
+  else {
+    // window width is less than 500px
+    function shrink_header() {
+      if (window.pageYOffset>115) {
+        header.style.top = "0px";
+        nav.style.top = "-29px";
+      }
+      if (window.pageYOffset<115) {
+        header.style.top = "-150px";
+        nav.style.top = "-150px";
+      }
+    }
+  }
+}
 
 // Sliding images----------------------------
 (function(){
